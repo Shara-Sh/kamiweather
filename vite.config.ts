fd6a0d4f599a49992b9b8d2a2d3a3de545506d20
Shara <shara.shahverdian@gmail.com>
2024-10-15 20:14:07 +0330

    features - add vitePWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [VitePWA({ registerType: "autoUpdate" }), react()],
   server: {
     host: "0.0.0.0",
   },
